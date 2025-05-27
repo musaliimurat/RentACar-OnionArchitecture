6dc8d966e1e89d375333d8c9af86a723858ced04
M M <musalimurat1@gmail.com>
2025-05-28 03:15:35 +0400

    update-28.05.2025/03:15

--- RentACar.MVC/ViewComponents/BlogViewComponents/_BlogDetailSidebarRecentBlogsComponentPartial.cs
+++ RentACar.MVC/ViewComponents/BlogViewComponents/_BlogDetailSidebarRecentBlogsComponentPartial.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentACar.Application.Interfaces.Services;
 
 namespace RentACar.MVC.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailSidebarRecentBlogsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IBlogService _blogService;
+
+        public _BlogDetailSidebarRecentBlogsComponentPartial(IBlogService blogService)
         {
-            return View();
+            _blogService = blogService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var recentBlogs = await _blogService.GetAllBlogIsNewAsync();
+            if (recentBlogs.Success)
+            {
+                return View(recentBlogs.Data);
+            }
+            return View("NoRecentBlogs");
+
         }
     }
 }
